77dca1cd44db460ed3f8c726d2d76f443711bb94
AllieStewart <megaxis1217@gmail.com>
2023-10-30 15:56:01 -0500

    added comments to server.js, for functions to add; need to update query.sql as well

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Start of JS file
 // Hosts the data for the terminal.
+// Require express and mySQL in order for application to function.
 const express = require('express');
-// Import and require mysql2
 const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
@@ -14,25 +14,44 @@ app.use(express.json());
 // Connect to database
 const db = mysql.createConnection(
     {
-        // host, username, password from my own info
-      host: '',
-      user: '',
-      password: '',
+        // Host, username, password from my own info
+      host: 'd6rii63wp64rsfb5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+      user: 'tgd0mxln1cijce41',
+      password: 'f78a0ymezkt8pv55',
       database: 'employees_db'
     },
     console.log(`Connected to the movies_db database.`)
   );
 
 // GET, POST, PUT, DELETE functions here.
+// GET all departments.
 
-// Default response for any other request (Not Found)
+// GET all roles.
+
+// GET all employees.
+
+// POST new department.
+
+// POST new role.
+
+// POST new employee.
+
+// PUT (update employee's role).
+
+// BONUS:
+// PUT (update employee's manager(s))
+// GET employee by manager
+// GET employee by department
+// DELETE departments, roles, employees.
+// GET total budget of department (aka combined salaries of all employees in department) 
+
+// Default response for any other request (Not Found).
 app.use((req, res) => {
     res.status(404).end();
   });
-  
+
+// App listening on port 3001.
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
-  
-
 // End of JS file
