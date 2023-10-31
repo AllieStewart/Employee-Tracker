f8f772cf317ebb1c719d8ab6e65cea78a61ea4dd
AllieStewart <megaxis1217@gmail.com>
2023-10-30 19:00:54 -0500

    added main inquirer prompts and updated get/post/put to be in their own functions, in server.js

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 // Start of JS file
 // Hosts the data for the terminal.
-// Require express and mySQL in order for application to function.
+// Require express, mySQL, and inquirer in order for application to function.
 const express = require('express');
 const mysql = require('mysql2');
-
+const inquirer = require('inquirer');
+// PORT and express object.
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// Express middleware
+// Express middleware.
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Connect to database
+// Connect to database.
 const db = mysql.createConnection(
     {
         // Host, username, password from my own info
@@ -20,62 +21,123 @@ const db = mysql.createConnection(
       password: 'f78a0ymezkt8pv55',
       database: 'employees_db'
     },
-    console.log(`Connected to the movies_db database.`)
+    console.log(`Connected to the employees_db database.`)
   );
 
-// GET, POST, PUT, DELETE functions here.
+// Inquirer prompts for user input.
+function run(){
+    return inquirer
+    .prompt([
+        {
+            type: 'list',
+            message: 'What would you like to do?',
+            name: 'selection',
+            choices: [
+                'View All Departments',
+                'View All Roles',
+                'View All Employees',
+                'Add Department',
+                'Add Role',
+                'Add Employee',
+                'Update Employee Role',
+                'Quit'
+            ]
+        }
+    ])
+    .then((answers) =>
+    {
+        switch (answers.selection) {
+            case 'View All Departments':
+                viewDepartments();
+                break;
+            case 'View All Roles':
+                viewRoles();
+                break;
+            case 'View All Employees':
+                viewEmployees();
+                break;
+            case 'Add Department':
+                addDept();
+                break;
+            case 'Add Role':
+                addRole();
+                break;
+            case 'Add Employee':
+                addEmployee();
+                break;
+            case'Update Employee Role':
+                updateEmpRole();
+                break;
+            default:
+                db.end();
+                break;
+    }
+})
+}
+
+// Start inquirer prompt(s).
+run();
+
 // GET all departments.
-app.get('/api/department', (req, res) => 
+function viewDepartments()
 {
-    const sql = `SELECT id, dept_name AS Depts FROM department`;
-  
-    db.query(sql, (err, rows) => {
-      if (err) {
-        res.status(500).json({ error: err.message });
-         return;
-      }
-      res.json({
-        message: 'success',
-        data: rows
-      });
+    app.get('/api/department', (req, res) => 
+    {
+        const sql = `SELECT id, dept_name AS Depts FROM department`;
+        db.query(sql, (err, rows) => {
+            if (err) {
+                res.status(500).json({ error: err.message });
+                return;
+            }
+            res.json({
+                message: 'success',
+                data: rows
+            });
+        });
     });
-});
+}
 
 // GET all roles.
-app.get('/api/role', (req, res) => 
+function viewRoles()
 {
-    const sql = `SELECT id, title AS Roles FROM role`;
-  
-    db.query(sql, (err, rows) => {
-      if (err) {
-        res.status(500).json({ error: err.message });
-         return;
-      }
-      res.json({
-        message: 'success',
-        data: rows
-      });
+    app.get('/api/role', (req, res) => 
+    {
+        const sql = `SELECT id, title AS Roles FROM role`;
+        db.query(sql, (err, rows) => {
+            if (err) {
+                res.status(500).json({ error: err.message });
+                return;
+            }
+            res.json({
+                message: 'success',
+                data: rows
+            });
+        });
     });
-});
+}
 
 // GET all employees.
-app.get('/api/employee', (req, res) => 
+function viewEmployees()
 {
-    const sql = `SELECT id, first_name, last_name AS Employees FROM employee`;
-  
-    db.query(sql, (err, rows) => {
-      if (err) {
-        res.status(500).json({ error: err.message });
-         return;
-      }
-      res.json({
-        message: 'success',
-        data: rows
-      });
+    app.get('/api/employee', (req, res) => 
+    {
+        const sql = `SELECT id, first_name, last_name AS Employees FROM employee`;
+        db.query(sql, (err, rows) => {
+            if (err) {
+                res.status(500).json({ error: err.message });
+                return;
+            }
+            res.json({
+                message: 'success',
+                data: rows
+            });
+        });
     });
-});
+}
 
 // POST new department.
+function addDept()
+{
 app.post('/api/new-department', ({ body }, res) => 
 {
     const sql = `INSERT INTO department (dept_name)
@@ -93,8 +155,11 @@ app.post('/api/new-department', ({ body }, res) =>
     });
   });
 });
+}
 
 // POST new role.
+function addRole()
+{
 app.post('/api/new-role', ({ body }, res) => 
 {
     const sql = `INSERT INTO role (role_name)
@@ -112,8 +177,11 @@ app.post('/api/new-role', ({ body }, res) =>
     });
   });
 });
+}
 
 // POST new employee.
+function addEmployee()
+{
 app.post('/api/new-employee', ({ body }, res) => 
 {
     const sql = `INSERT INTO employee (first_name, last_name)
@@ -131,8 +199,11 @@ app.post('/api/new-employee', ({ body }, res) =>
     });
   });
 });
+}
 
 // PUT (update employee's role).
+function updateEmpRole()
+{
 app.put('/api/employee/:role_id', (req, res) => {
     const sql = `UPDATE employee SET first_name = ?, last_name = ? WHERE role_id = ?`;
     const params = [req.body.first_name, req.body.last_name, req.params.role_id];
@@ -153,6 +224,7 @@ app.put('/api/employee/:role_id', (req, res) => {
       }
     });
   });
+}
   
 // BONUS:
 // PUT (update employee's manager(s))
